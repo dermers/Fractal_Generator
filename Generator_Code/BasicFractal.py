c2f4a45f9781fd19550b86c35614272121a4f096
Shawn TheGuy <shawn.dermer@gmail.com>
2019-05-30 20:31:23 -0400

    tweaks

--- Generator_Code/BasicFractal.py
+++ Generator_Code/BasicFractal.py
@@ -1,12 +1,9 @@
 from turtle import RawTurtle, TurtleScreen
 from tkinter import Tk, Canvas
 
-# A point is a tuple of (float, float)
-# A line is a tuple of (point, point)
-
 WIDTH = 1200
 HEIGHT = 675
-START_POS = (-WIDTH / 2), HEIGHT / 2
+START_POS = (-WIDTH / 2), (-HEIGHT / 2) + HEIGHT / 20
 # print(START_POS)
 
 root = Tk()
@@ -20,25 +17,25 @@ canvas.pack()
 # set up turtle
 turt = RawTurtle(turtle_screen)
 turt.hideturtle()
+turt.speed(0)
 turt.pencolor("RED")
 turt.width(3)
 turt.up()
 turt.setx(START_POS[0])
 turt.sety(START_POS[1])
 turt.down()
-turt.speed(0)
 
 farthest_pos = [0, 0]
 
 def update_pos():
     tpos = turt.pos()
-    pos = [tpos[0] - START_POS[0], tpos[1] + START_POS[1]]
+    pos = [tpos[0] - START_POS[0], tpos[1] - START_POS[1]]
     if abs(pos[0]) > abs(farthest_pos[0]):
         farthest_pos[0] = pos[0]
     if abs(pos[1]) > abs(farthest_pos[1]):
         farthest_pos[1] = pos[1]
 
-def draw_fractal(n):
+def draw_fractal(n = 0):
     if n <= 0:
         turt.forward(5)
         update_pos()
@@ -61,9 +58,9 @@ def draw_fractal(n):
         draw_fractal(n - 1)
 
 
-draw_fractal(4)
+draw_fractal(1)
 print(farthest_pos)
-canvas.scale("all", START_POS[0], START_POS[1],
-             WIDTH / farthest_pos[0], HEIGHT / farthest_pos[1])
+canvas.scale("all", START_POS[0], -START_POS[1],
+             WIDTH / farthest_pos[0], (HEIGHT / farthest_pos[1]) / 1.1)
 
 root.mainloop()
